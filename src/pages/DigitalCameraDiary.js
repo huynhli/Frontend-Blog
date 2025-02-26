691252084870700a90b7032b74f571758096441c
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-02-26 11:30:13 -0500

    Refactor
    refactored and organized project files
    
    - update for md files
    - write blog posts
    - update projects page

--- src/pages/DigitalCameraDiary.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function DigitalCameraDiary () {
-   return (
-      <div>
-         <h1>Add a slideshow for each album of all the pics, plus a grid view of all photos in that album</h1>
-      </div>
-   )
-}
--- src/pages/Socials.js
+++
@@ -1,7 +0,0 @@
-export default function Socials (){
-   return(
-      <div>
-         <h1>Hi</h1>
-      </div>
-   )
-}
